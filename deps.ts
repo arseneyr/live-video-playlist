a6f381dd53d0d0f97e4b4293e156e81f5ab29e2a
arseneyr <arseneyr@users.noreply.github.com>
2022-05-18 00:37:24 +0000

    more invalid tests

--- deps.ts
+++ deps.ts
@@ -1,2 +1,3 @@
 export { default as HLS } from "https://esm.sh/hls-parser@0.10.4?pin=v78";
 export * as asserts from "https://deno.land/std@0.138.0/testing/asserts.ts";
+export { delay } from "https://deno.land/std@0.139.0/async/mod.ts";
--- main.test.ts
+++ main.test.ts
@@ -1,12 +1,65 @@
-import { asserts } from "./deps.ts";
+import { asserts, delay, HLS } from "./deps.ts";
 import livePlaylist from "./main.ts";
 
-Deno.test("invalid target duration", async () => {
+const samplePlaylist = HLS.stringify(
+  new HLS.types.MediaPlaylist({
+    targetDuration: 1,
+    segments: [
+      new HLS.types.Segment({
+        uri: "yo",
+        duration: 1,
+        mediaSequenceNumber: 0,
+        discontinuitySequence: 0,
+      }),
+    ],
+  }),
+);
+
+Deno.test("invalid target duration", () => {
   const createFakeOptions = (targetDuration: number) => ({
     getNextVideo: () => ({ hlsPlaylist: "" }),
     fallbackPlaceholder: "",
     targetDuration,
   });
-  await asserts.assertRejects(() => livePlaylist(createFakeOptions(0)));
-  await asserts.assertRejects(() => livePlaylist(createFakeOptions(1.5)));
+  asserts.assertThrows(
+    () => livePlaylist(createFakeOptions(0)),
+    Error,
+    "target duration",
+  );
+  asserts.assertThrows(
+    () => livePlaylist(createFakeOptions(1.5)),
+    Error,
+    "target duration",
+  );
+});
+
+Deno.test("invalid placeholder playlist", () => {
+  const createFakeOptions = () => ({
+    getNextVideo: () => ({ hlsPlaylist: "" }),
+    fallbackPlaceholder: {
+      hlsPlaylist: "",
+    },
+    targetDuration: 1,
+  });
+  asserts.assertThrows(
+    () => livePlaylist(createFakeOptions()),
+    Error,
+    "placeholder",
+  );
+});
+Deno.test("pending getNextVideo without placeholder", async () => {
+  const nextVideoPromise = delay(0).then(() => ({
+    hlsPlaylist: samplePlaylist,
+  }));
+  const createFakeOptions = () => ({
+    getNextVideo: async () => ({ hlsPlaylist: samplePlaylist }),
+    targetDuration: 1,
+  });
+  const { start } = livePlaylist(createFakeOptions());
+  await asserts.assertRejects(
+    start,
+    Error,
+    "pending",
+  );
+  await nextVideoPromise;
 });
